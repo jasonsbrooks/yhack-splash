a51dc3d4cb27bdc91dec005acc54277e64ca78c9
jasonsbrooks <jason.brooks@yale.edu>
2014-04-24 22:32:47 -0400

    App structure

---
+++ scripts/code.js
@@ -0,0 +1,8 @@
+/*::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::::::: Kirill Miniaev | Miniaev Design ::::::::*/
+/*: kirill@miniaevdesign.com | miniaevdesign.com :*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+$(document).ready(function() {
+	//alert('test');
+});
